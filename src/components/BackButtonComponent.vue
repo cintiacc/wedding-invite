ee4c2777566d890e20b03f0c24f2b9d19c3ee58a
Cíntia Cunha <90116425+cintiacc@users.noreply.github.com>
2025-05-29 23:33:29 -0300

    update layout

---
+++ src/components/BackButtonComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="goBack" class="mr-3">
+      <span class="material-symbols-outlined">arrow_back</span>
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function goBack() {
+  if (window.history.length > 1) {
+    router.back();
+  } else {
+    router.push('/home'); // rota fallback, ajuste conforme seu app
+  }
+}
+</script>
