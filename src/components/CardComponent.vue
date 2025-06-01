8ecdc867d9eac1acfa5e47934b296627b9c24f56
Cíntia Cunha <90116425+cintiacc@users.noreply.github.com>
2025-05-31 21:49:11 -0300

    add gifts view

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex items-center bg-white rounded-2xl shadow-md overflow-hidden p-4 w-full max-w-md mx-auto h-40">
+    <!-- Imagem -->
+    <div class="w-16 h-16 flex-shrink-0 rounded-xl overflow-hidden">
+      <img :src="imageSrc" alt="Thumbnail" class="object-cover w-full h-full" />
+    </div>
+
+    <!-- Conteúdo -->
+    <div class="ml-4 flex flex-col justify-between w-full">
+      <div>
+        <h2 class="text-base font-semibold text-gray-800">{{ title }}</h2>
+        <p class="text-xs text-gray-500">{{ subtitle }}</p>
+      </div>
+      <div class="mt-2">
+        <ButtonComponent>
+          Dar presente
+        </ButtonComponent>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonComponent from './ButtonComponent.vue';
+
+export default {
+  name: 'CardComponent',
+  components: {
+    ButtonComponent,
+  },
+  props: {
+    imageSrc: String,
+    title: String,
+    subtitle: String
+  }
+};
+</script>
---
+++ src/views/Gifts.vue
@@ -0,0 +1,70 @@
+<template>
+<div class="min-h-screen flex items-center justify-center py-10 px-4">
+  <div class="min-h-screen">    
+    <div class="flex items-center mb-6">
+        <BackButtonComponent />
+        <h2 class="text-3xl font-bold text-rose-800 mb-4">Lista de presentes</h2>
+    </div>
+    <div class="grid md:grid-cols-3 gap-6 max-w-5xl mx-auto">
+      <CardComponent
+        v-for="(card, index) in cards"
+        :key="index"
+        :imageSrc="card.imageSrc"
+        :title="card.title"
+        :subtitle="card.subtitle"
+        :itemCount="card.itemCount"
+      />
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import CardComponent from '../components/CardComponent.vue'
+import BackButtonComponent from '../components/BackButtonComponent.vue'
+
+export default {
+  components: { CardComponent, BackButtonComponent },
+  data() {
+    return {
+      cards: [
+        {
+          imageSrc: 'https://via.placeholder.com/150',
+          title: 'Jogo de panelas antiaderente',
+          subtitle: '',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/150',
+          title: "Kit de utensílios de cozinha",
+          subtitle: '',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/150',
+          title: 'Kit de taças',
+          subtitle: '',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/150',
+          title: 'Jogo de cama ou colcha de casal',
+          subtitle: '',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/150',
+          title: 'Tapete para sala ou quarto',
+          subtitle: '',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/150',
+          title: 'Faqueiro Inox',
+          subtitle: '',
+        },
+        {
+          imageSrc: 'https://via.placeholder.com/150',
+          title: 'Toalhas de banho',
+          subtitle: '',
+        },
+      ]
+    }
+  }
+}
+</script>
